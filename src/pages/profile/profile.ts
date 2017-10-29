9ae8fc1914be27b85e0a39bd82625b8c58f74ab0
aagavin <aagavin@gmail.com>
2017-10-28 20:52:07 -0400

    Propper case for class name

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,25 +1,21 @@
 import { Component } from '@angular/core';
 import { NavParams, IonicPage } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
-import { userService } from '../../services/userService';
+import { UserService } from '../../services/userService';
 import 'rxjs/add/operator/map';
-/*
-  Generated class for the Profile page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
-  providers: [userService]
+  providers: [UserService]
 })
 export class ProfilePage {
   public user: Object;
   constructor(public navParams: NavParams,
     private storage: Storage,
-    private userService: userService) {
+    private userService: UserService) {
     this.user = new Object();
   }
   ionViewDidLoad() {
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,18 +3,24 @@ import { Storage } from '@ionic/storage';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class userService {
+export class UserService {
     public orders: Array<Object>
-    
+
     constructor(
         private http: Http,
         public request: RequestOptions,
         public storage: Storage,
         //public headers: Headers
-    ) {        
+    ) {
         this.orders = new Array<Object>()
     }
-    //Gets Users info and stores it locally so the information can be used throughout the app
+    
+
+    /**
+     * Gets Users info and stores it locally so the information can be used throughout the app
+     * 
+     * @memberof userService
+     */
     public GetUserInfo(): void {
         //This is where we get the token for the backend validation
         this.storage.get('token').then(value => {
@@ -34,18 +40,25 @@ export class userService {
             )
         })
     }
-        public GetOrderHistory(): void {
-            this.storage.get('token').then(value => {
-                let headers = new Headers();
-                headers.append('token', value)    
-                let options = new RequestOptions({ headers: headers });
-                this.http.get('https://keanubackend.herokuapp.com/order', options).map(res => res.json()).subscribe(
-                    data => {
-                        this.orders = data.data.orders;
-                    }, err => {
-                        console.log(err);
-                    },
-                )
-            })
-        }
+
+
+    /**
+     * Returns a users order history 
+     * 
+     * @memberof userService
+     */
+    public GetOrderHistory(): void {
+        this.storage.get('token').then(value => {
+            let headers = new Headers();
+            headers.append('token', value)
+            let options = new RequestOptions({ headers: headers });
+            this.http.get('https://keanubackend.herokuapp.com/order', options).map(res => res.json()).subscribe(
+                data => {
+                    this.orders = data.data.orders;
+                }, err => {
+                    console.log(err);
+                },
+            )
+        })
+    }
 }
