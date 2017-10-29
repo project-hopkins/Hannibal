417acb53c4959c66b384c04279b55f2a7a6ab7de
aagavin <aagavin@gmail.com>
2017-10-28 20:51:44 -0400

    moved home page providers to home module

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,14 +1,17 @@
+import {MenuCallService} from '../../services/getMenu';
 //home.module.ts
 import { NgModule } from '@angular/core';
-import { HomePage} from './home';
+import { HomePage } from './home';
 import { IonicPageModule } from 'ionic-angular';
+import { UserService } from '../../services/userService';
 
 @NgModule({
   declarations: [HomePage],
   imports: [IonicPageModule.forChild(HomePage),
-],
+  ],
+  providers: [MenuCallService, UserService],
   entryComponents: [
-      HomePage
+    HomePage
   ]
 })
 export class HomePageModule { }
