60aa2360bea62e7c25dade9870c3ee7522760270
aagavin <aagavin@gmail.com>
2017-11-06 12:31:54 -0500

    optimed imports add userservice as global service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import {OneSignal} from '@ionic-native/onesignal';
-import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from '@angular/platform-browser';
+import { OneSignal } from '@ionic-native/onesignal';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { IonicStorageModule } from '@ionic/storage'
+import { UserService } from '../services/userService';
 import { MyApp } from './app.component';
 
 @NgModule({
@@ -26,6 +27,7 @@ import { MyApp } from './app.component';
   providers: [{provide: ErrorHandler, 
     useClass: IonicErrorHandler}, 
     OneSignal,
+    UserService,
     StatusBar,
     SplashScreen
   ]
