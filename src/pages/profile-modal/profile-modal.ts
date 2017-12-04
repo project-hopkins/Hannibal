9e5b15f8888bb77808949fee2755c7ca6584b20a
Omid <maverickxm2@live.ca>
2017-12-04 15:24:18 -0500

    just saves for branch checkout

--- src/pages/profile-modal/profile-modal.ts
+++ src/pages/profile-modal/profile-modal.ts
@@ -1,32 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, IonicPage, ViewController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { UserService } from '../../services/userService';
+
 
 @IonicPage()
 @Component({
   selector: 'page-profile-modal',
   templateUrl: 'profile-modal.html',
+  providers: [UserService]  
 })
 export class ProfileModalPage {
+  public user: Object;  
   public title = 'Hello';
   public informationType: string = this.navParams.get('informationType');
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
-    public viewCtrl: ViewController) {
+    private storage: Storage,    
+    public viewCtrl: ViewController,
+    private userService: UserService) {
     console.log(this.informationType);
-     
     // Get the user info and have it as a placeholder in the text fields
-    
   }
 
 
-  ionViewDidLoad() {
-    //console.log(this.navParams.get('informationType'));
+  ionViewDidLoad() {  
+    //customer/profile/edit
+    
+    this.userService.getUserInfo()
+    console.log('ionViewDidLoad HomePage');
+    this.storage.get('token').then((value: string) => {
+      console.log("STARTED");
+      console.log(value)
+    })
+    
   }
 
   public updateInformation(){
     // Code to edit information.
+
+    this.storage.get('userFullDetails').then((val) => {
+      this.user = val;
+      console.log(this.user);
+      // Have to do it in here
+      // Example of how to get invidual values from it
+      console.log(this.user['address'].name);
+    });
+
+
 
     // Check Swagger to see call post call details
     // Token in header
